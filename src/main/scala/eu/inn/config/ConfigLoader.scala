50fb6ba2ab3cb35e8b009c95a937950caaa33182
Maga Abdurakhmanov <maga@inn.eu>
2015-11-28 00:02:44 +0100

    - load should return Config, + object ConfigLoader

--- src/main/scala/eu/inn/config/ConfigLoader.scala
+++ src/main/scala/eu/inn/config/ConfigLoader.scala
@@ -2,16 +2,23 @@ package eu.inn.config
 
 import java.io.File
 
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{Config, ConfigFactory}
 
-class ConfigLoader(localConfigPropertyName: String = "config.localfile", separator: String = File.pathSeparator) {
-  def load() = {
-    val conf = System.getProperty(localConfigPropertyName, "")
+class ConfigLoader(localConfigPropertyName: String, separator: String) {
+  def load(): Config = {
+    System.getProperty(localConfigPropertyName, "")
       .split(separator)
       .foldLeft(ConfigFactory.load())({ (conf, filePath) ⇒
         val file = new java.io.File(filePath.trim)
         ConfigFactory.parseFile(file).withFallback(conf)
       })
       .resolve()
+  }
+}
+
+object ConfigLoader {
+  def apply(localConfigPropertyName: String = "config.localfile", separator: String = File.pathSeparator): Config = {
+    val loader = new ConfigLoader(localConfigPropertyName, separator)
+    loader.load()
   }
 }
