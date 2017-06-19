297593561fd174b57e640d62e289a916ac273541
Maga Abdurakhmanov <maqdev@gmail.com>
2017-06-19 19:12:57 +0300

    + allow to provide config file names directly and empty defaults

--- src/main/scala/com/hypertino/service/config/ConfigLoader.scala
+++ src/main/scala/com/hypertino/service/config/ConfigLoader.scala
@@ -4,27 +4,38 @@ import java.io.{File, FileNotFoundException}
 
 import com.typesafe.config.{Config, ConfigFactory}
 
-class ConfigLoader(localConfigPropertyName: String, separator: String, failIfConfigNotFound: Boolean) {
-  def load(): Config = {
-    System.getProperty(localConfigPropertyName, "")
-      .split(separator)
-      .filterNot(_.trim.isEmpty)
-      .foldLeft(ConfigFactory.load())({ (conf, filePath) ⇒
-        val file = new java.io.File(filePath.trim)
-        if (!file.exists && failIfConfigNotFound) {
-          throw new FileNotFoundException(s"${file.getAbsolutePath} is not found")
-        }
-        ConfigFactory.parseFile(file).withFallback(conf)
-      })
-      .resolve()
-  }
-}
-
 object ConfigLoader {
   def apply(localConfigPropertyName: String = "config.localfile",
             separator: String = File.pathSeparator,
-            failIfConfigNotFound: Boolean = true): Config = {
-    val loader = new ConfigLoader(localConfigPropertyName, separator, failIfConfigNotFound)
-    loader.load()
+            failIfConfigNotFound: Boolean = true,
+            loadDefaults: Boolean = true
+           ): Config = {
+
+
+    apply(
+      System.getProperty(localConfigPropertyName, "")
+        .split(separator)
+        .filterNot(_.trim.isEmpty),
+      failIfConfigNotFound,
+      loadDefaults
+    )
+  }
+
+  def apply(configFiles: Seq[String],
+            failIfConfigNotFound: Boolean,
+            loadDefaults: Boolean): Config = {
+
+    val defaults = if(loadDefaults)
+      ConfigFactory.load()
+    else
+      ConfigFactory.empty()
+
+    configFiles.foldLeft(defaults)({ (conf, filePath) ⇒
+      val file = new java.io.File(filePath.trim)
+      if (!file.exists && failIfConfigNotFound) {
+        throw new FileNotFoundException(s"${file.getAbsolutePath} is not found")
+      }
+      ConfigFactory.parseFile(file).withFallback(conf)
+    }).resolve()
   }
 }
--- src/test/scala/TestConfigLoader.scala
+++ src/test/scala/TestConfigLoader.scala
@@ -1,6 +1,7 @@
 import java.io.{File, IOException}
 
 import com.hypertino.service.config.ConfigLoader
+import com.typesafe.config.ConfigException
 import org.scalatest.{FreeSpec, Matchers}
 
 class TestConfigLoader extends FreeSpec with Matchers {
@@ -9,18 +10,37 @@ class TestConfigLoader extends FreeSpec with Matchers {
     config.getString("test-value") shouldBe "100500"
   }
 
-  "ConfigLoader should load config + command-line-files" in {
+  "ConfigLoader should load config + command-line-files with system properties" in {
     System.setProperty("test-configs", "./testdata/test-1.conf" + File.pathSeparator + "./testdata/test-2.conf")
     val config = ConfigLoader(localConfigPropertyName = "test-configs")
     config.getString("test-value") shouldBe "shwonder"
     config.getString("test-name") shouldBe "Abraham"
   }
 
-  "ConfigLoader should fail if config isn't found" in {
+  "ConfigLoader should load config + command-line-files directly" in {
+    val config = ConfigLoader(Seq("./testdata/test-1.conf", "./testdata/test-2.conf"),true,true)
+    config.getString("test-value") shouldBe "shwonder"
+    config.getString("test-name") shouldBe "Abraham"
+  }
+
+  "ConfigLoader should fail if config isn't found with system properties" in {
     System.setProperty("test-configs", "./testdata/not-existing.conf" + File.pathSeparator + "./testdata/test-2.conf")
 
     intercept[IOException] {
       ConfigLoader(localConfigPropertyName = "test-configs")
+    }
+  }
+
+  "ConfigLoader should fail if config isn't found" in {
+    intercept[IOException] {
+      ConfigLoader(Seq("./testdata/not-existing.conf"),true,true)
+    }
+  }
+
+  "ConfigLoader should not load default config" in {
+    val config = ConfigLoader(loadDefaults=false)
+    intercept[ConfigException.Missing] {
+      config.getString("test-value") shouldBe "100500"
     }
   }
 }
