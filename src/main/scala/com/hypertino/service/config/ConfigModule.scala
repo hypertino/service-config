32b29f5653f19079e4bbd6bef1be5bd0249de421
Maga Abdurakhmanov <maqdev@gmail.com>
2017-07-20 16:19:25 +0200

    + ConfigModule

--- src/main/scala/com/hypertino/service/config/ConfigModule.scala
+++ src/main/scala/com/hypertino/service/config/ConfigModule.scala
@@ -1,18 +1,26 @@
 package com.hypertino.service.config
 
+import java.io.File
+
 import com.typesafe.config.Config
 import scaldi.{Injector, Module}
 
-class ConfigModule(localConfigPropertyName: String = "config.localfile") extends Module {
-  val rootConfig = ConfigLoader(localConfigPropertyName)
+class ConfigModule(localConfigPropertyName: String,
+                   separator: String,
+                   failIfConfigNotFound: Boolean,
+                   loadDefaults: Boolean) extends Module {
+  val rootConfig = ConfigLoader(localConfigPropertyName, separator, failIfConfigNotFound, loadDefaults)
   bind[Config] identifiedBy 'config toNonLazy rootConfig
 }
 
 object ConfigModule {
   def apply(localConfigPropertyName: String = "config.localfile",
+            separator: String = File.pathSeparator,
+            failIfConfigNotFound: Boolean = true,
+            loadDefaults: Boolean = true,
             injectModulesConfigPath: Option[String] = Some("inject-modules")): Injector = {
 
-    val configModule = new ConfigModule(localConfigPropertyName)
+    val configModule = new ConfigModule(localConfigPropertyName, separator, failIfConfigNotFound, loadDefaults)
     loadConfigInjectedModules(configModule.rootConfig, injectModulesConfigPath, configModule)
   }
 
